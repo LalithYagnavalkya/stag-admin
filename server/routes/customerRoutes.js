ffd640f00a0f16dccb8472016dcfd02ef556bdf2
lalithyagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-01-11 17:55:35 +0530

    add customer routes

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -8,7 +8,21 @@ const {
   updateReturns,
   updateCapital,
 } = require("../controllers/customerController");
+const auth = require("../middleware/auth");
 
 const customerRouter = express.Router();
+// customerRouter.post("/", auth,createCustomer);
+
+customerRouter.get("/getallcustomers", auth, getCustomers);
+
+customerRouter.patch("/:id", auth, getCustomer);
+
+customerRouter.delete("/:id", auth, deleteCustomer);
+
+customerRouter.put("/:id", auth, updateCustomer);
+
+customerRouter.patch("/:id", auth, updateCapital);
+
+customerRouter.patch("/:id", auth, updateReturns);
 
 module.exports = customerRouter;
