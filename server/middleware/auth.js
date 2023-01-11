c1f81cda7c97ba46b41beb9f182c87f297bccadd
lalithyagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-01-11 17:56:02 +0530

    Fix middleware token auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   try {
-    let token = req.header.authorization;
+    let token = req.headers.authorization;
+    console.log(token);
     if (token) {
       token = token.split(" ")[1];
       let user = jwt.verify(token, process.env.JWT_SECRET);
