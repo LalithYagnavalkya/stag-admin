f26248a6f7c3cfe8ec51ef6464320ab88a57424d
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-01-11 08:24:31 +0530

    Add auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  try {
+    let token = req.header.authorization;
+    if (token) {
+      token = token.split(" ")[1];
+      let user = jwt.verify(token, process.env.JWT_SECRET);
+      req.userId = user.id;
+    } else {
+      res.status(401).json({ message: "Unauthorized User" });
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ message: "Unauthorized User" });
+  }
+};
+
+module.exports = auth;
