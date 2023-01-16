ca4ae68832d870ac3ea3cf7ba6e6d94f065f3f30
lalithyagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-01-16 10:08:49 +0530

    update baseurl

--- app/src/baseUrl.js
+++ app/src/baseUrl.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const baseUrl = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_PORT,
-  // baseURL: "https://campus-backend-drf-bfr8p.ondigitalocean.app/",
-  // baseURL: 'http://13.233.194.93:8000/',
-  // baseURL: 'https://hammerhead-app-4smkg.ondigitalocean.app/',
-  // baseURL: 'https://api-campus.herokuapp.com',
 });
 
 export default baseUrl;
